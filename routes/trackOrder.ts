4bf446b1920b074214949cd85ad282daf9585bae
tobifortisec <tobi@fortisec.io>
2026-07-29 00:41:29 +0100

    Update trackOrder.ts

--- routes/trackOrder.ts
+++ routes/trackOrder.ts
@@ -13,7 +13,6 @@ const db = require('../data/mongodb')
 module.exports = function trackOrder () {
   return (req: Request, res: Response) => {
     const id = utils.disableOnContainerEnv() ? String(req.params.id).replace(/[^\w-]+/g, '') : req.params.id
-
     challengeUtils.solveIf(challenges.reflectedXssChallenge, () => { return utils.contains(id, '<iframe src="javascript:alert(`xss`)">') })
     db.orders.find({ $where: `this.orderId === '${id}'` }).then((order: any) => {
       const result = utils.queryResultToJson(order)
